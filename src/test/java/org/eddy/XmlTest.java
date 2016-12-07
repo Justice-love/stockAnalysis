afef4ab9ee5d7104c92fefd9cb26a02e989bf78c
eddy <eddyxu1213@126.com>
2016-12-07 23:12:39 +0800

    解析xml

--- src/test/java/org/eddy/XmlTest.java
+++ src/test/java/org/eddy/XmlTest.java
@@ -2,7 +2,11 @@ package org.eddy;
 
 import org.eddy.exception.JsoupException;
 import org.eddy.xml.XmlContext;
+import org.eddy.xml.entity.Url;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * Created by eddy on 2016/12/6.
@@ -12,6 +16,7 @@ public class XmlTest {
     @Test
     public void test() throws JsoupException {
         XmlContext context = new XmlContext();
-        context.loadXml("configration.xml");
+        List<Url> urlList = context.loadXml("configration.xml");
+        Assert.assertEquals(1, urlList.size());
     }
 }
