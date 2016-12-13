9dbb57e63b792508fd4c8b7691f54dd2dcb153f8
justice-eddy <eddy.xu@oceanwing.com>
2016-12-13 13:35:36 +0800

    自动数据统计定时任务

--- web/src/main/java/org/eddy/schedule/crawlUrl.java
+++ web/src/main/java/org/eddy/schedule/crawlUrl.java
@@ -1,7 +1,10 @@
 package org.eddy.schedule;
 
+import org.eddy.manager.ComputerAndLoadHistoryStock;
 import org.eddy.manager.CrawlStockManager;
 import org.jsoup.nodes.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -16,15 +19,28 @@ import org.springframework.stereotype.Component;
 //@EnableScheduling
 public class crawlUrl {
 
+    private static final Logger logger = LoggerFactory.getLogger(crawlUrl.class);
+
     @Autowired
     private CrawlStockManager crawlStockManager;
+
+    @Autowired
+    private ComputerAndLoadHistoryStock computerAndLoadHistoryStock;
 
     @Scheduled(fixedDelay = 60_000,initialDelay = 3_000)
     public void crawlAllUrl() {
         try {
             crawlStockManager.crawlStock();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("execute Schedule crawlAllUrl error", e);
+        }
+    }
+
+    public void computer() {
+        try {
+            computerAndLoadHistoryStock.computerAndLoad();
+        } catch (Exception e) {
+            logger.error("execute Schedule computer error", e);
         }
     }
 
