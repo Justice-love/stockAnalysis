89d037b12d5fe4d81b19aee64a1074c79ba4313a
justice-eddy <eddy.xu@oceanwing.com>
2016-12-16 17:20:49 +0800

    修改模版，增加service注解

--- web/src/main/java/org/eddy/manager/impl/StockBuyManagerImpl.java
+++ web/src/main/java/org/eddy/manager/impl/StockBuyManagerImpl.java
@@ -2,16 +2,14 @@ package org.eddy.manager.impl;
 
 import org.eddy.entity.Stock;
 import org.eddy.manager.StockBuyManager;
-import org.eddy.service.NotifyService;
 import org.eddy.service.StockService;
-import org.eddy.service.StockWantBuyService;
 import org.eddy.swing.SwingContext;
 import org.eddy.swing.entity.Swing;
 import org.eddy.swing.flow.SwingFlow;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.SortedMap;
@@ -21,6 +19,7 @@ import java.util.stream.Collectors;
 /**
  * Created by eddy on 16/12/16.
  */
+@Service
 public class StockBuyManagerImpl implements StockBuyManager {
 
     private static final Logger logger = LoggerFactory.getLogger(StockBuyManagerImpl.class);
