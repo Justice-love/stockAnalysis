1495bc44dbd5d65e0d1c34699e81d72185e1f149
eddy <eddyxu1213@126.com>
2017-03-04 23:19:26 +0800

    增加卖出IM模板

--- swing/src/main/java/org/eddy/swing/util/MarkdownUtil.java
+++ swing/src/main/java/org/eddy/swing/util/MarkdownUtil.java
@@ -2,29 +2,30 @@ package org.eddy.swing.util;
 
 import org.eddy.swing.entity.SwingValidateContext;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 /**
- * Created by Justice-love on 2017/3/4.
+ >* Created by Justice-love on 2017/3/4.
  */
 public class MarkdownUtil {
 
     public static String createMarkdownBuyInContent(SwingValidateContext context) {
         Objects.requireNonNull(context.getStock(), "context.getStock() is null");
         StringBuilder builder = new StringBuilder("## 股票购买提醒\n");
-        builder.append("#### 基本信息\n");
-        builder.append("* 股票名称：").append(context.getStock().getName()).append("\n");
-        builder.append("* 股票代码：").append(context.getStock().getStockCode()).append("\n");
-        builder.append("* 当前价格：").append(context.getStock().getPrice()).append("\n");
-        builder.append("* 当前涨幅：").append(context.getStock().getUp()).append("\n");
-        builder.append("* 当前日期：").append(context.getStock().getDate()).append("\n");
-        builder.append("* 当前时间：").append(context.getStock().getTime()).append("\n");
-        builder.append("* 推送时间：").append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))).append("\n");
-        builder.append("* 通过规则：\n");
+        builder.append("\n### 基本信息\n");
+        builder.append(">* 股票名称：").append(context.getStock().getName()).append("\n");
+        builder.append(">* 股票代码：").append(context.getStock().getStockCode()).append("\n");
+        builder.append(">* 当前价格：").append(context.getStock().getPrice()).append("\n");
+        builder.append(">* 当前涨幅：").append(context.getStock().getUp()).append("\n");
+        builder.append(">* 当前日期：").append(context.getStock().getDate()).append("\n");
+        builder.append(">* 当前时间：").append(context.getStock().getTime()).append("\n");
+        builder.append(">* 推送时间：").append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))).append("\n");
+        builder.append(">* 通过规则：\n");
         context.getFlowSwings().stream().forEach(s -> {
-            builder.append("\t* ")
+            builder.append("\t>* ")
                     .append(s.getSwing().getValidateType().name())
                     .append(" (")
                     .append(s.getSwing().getValidateType().getDescription())
@@ -32,9 +33,10 @@ public class MarkdownUtil {
                     .append(s.isSuccess())
                     .append("\n");
         });
-        builder.append("#### 趋势图\n");
+        
+        builder.append("\n### 趋势图\n");
         builder.append("![趋势图](http://image.sinajs.cn/newchart/daily/n/").append(context.getStock().getStockCode()).append(".gif)\n");
-        builder.append("#### 分时线\n");
+        builder.append("\n### 分时线\n");
         builder.append("![分时线](http://image.sinajs.cn/newchart/min/n/").append(context.getStock().getStockCode()).append(".gif)\n");
         return  builder.toString();
     }
@@ -42,17 +44,17 @@ public class MarkdownUtil {
     public static String createMarkdownSaleOutContent(SwingValidateContext context) {
         Objects.requireNonNull(context.getStock(), "context.getStock() is null");
         StringBuilder builder = new StringBuilder("## 股票卖出提醒\n");
-        builder.append("#### 基本信息\n");
-        builder.append("* 股票名称：").append(context.getStock().getName()).append("\n");
-        builder.append("* 股票代码：").append(context.getStock().getStockCode()).append("\n");
-        builder.append("* 当前价格：").append(context.getStock().getPrice()).append("\n");
-        builder.append("* 当前涨幅：").append(context.getStock().getUp()).append("\n");
-        builder.append("* 当前日期：").append(context.getStock().getDate()).append("\n");
-        builder.append("* 当前时间：").append(context.getStock().getTime()).append("\n");
-        builder.append("* 推送时间：").append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))).append("\n");
-        builder.append("* 通过规则：\n");
+        builder.append("\n### 基本信息\n");
+        builder.append(">* 股票名称：").append(context.getStock().getName()).append("\n");
+        builder.append(">* 股票代码：").append(context.getStock().getStockCode()).append("\n");
+        builder.append(">* 当前价格：").append(context.getStock().getPrice()).append("\n");
+        builder.append(">* 当前涨幅：").append(context.getStock().getUp()).append("\n");
+        builder.append(">* 当前日期：").append(context.getStock().getDate()).append("\n");
+        builder.append(">* 当前时间：").append(context.getStock().getTime()).append("\n");
+        builder.append(">* 推送时间：").append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))).append("\n");
+        builder.append(">* 通过规则：\n");
         context.getFlowSwings().stream().forEach(s -> {
-            builder.append("\t* ")
+            builder.append("\t>* ")
                     .append(s.getSwing().getValidateType().name())
                     .append(" (")
                     .append(s.getSwing().getValidateType().getDescription())
@@ -60,12 +62,14 @@ public class MarkdownUtil {
                     .append(s.isSuccess())
                     .append("\n");
         });
-        builder.append("#### 购买信息\n");
-        builder.append("* 购买价格：").append(context.getStock().getBoughtPrice()).append("\n");
-        builder.append("* 购买时间：").append(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(context.getStock().getBoughtTime().toInstant())).append("\n");
-        builder.append("#### 趋势图\n");
+        
+        builder.append("\n### 购买信息\n");
+        builder.append(">* 购买价格：").append(context.getStock().getBoughtPrice()).append("\n");
+        builder.append(">* 购买时间：").append(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(context.getStock().getBoughtTime())).append("\n");
+        
+        builder.append("\n### 趋势图\n");
         builder.append("![趋势图](http://image.sinajs.cn/newchart/daily/n/").append(context.getStock().getStockCode()).append(".gif)\n");
-        builder.append("#### 分时线\n");
+        builder.append("\n### 分时线\n");
         builder.append("![分时线](http://image.sinajs.cn/newchart/min/n/").append(context.getStock().getStockCode()).append(".gif)\n");
         return  builder.toString();
     }
--- web/src/test/java/org/eddy/test/http/HttpUtilTest.java
+++ web/src/test/java/org/eddy/test/http/HttpUtilTest.java
@@ -19,6 +19,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * Created by Justice-love on 2017/3/4.
@@ -64,5 +65,28 @@ public class HttpUtilTest {
         boolean result = HttpUtil.sendMsg(message);
         Assert.assertTrue(result);
 
+    }
+
+    @Test
+    public void test3() throws IOException {
+        ValidateSwing validateSwing = new ValidateSwing(new Swing("id", "test", null, new BuyPrice()));
+        validateSwing.setSuccess(true);
+        Stock stock = new Stock();
+        stock.setStockCode("sh601006");
+        stock.setName("大秦铁路");
+        stock.setPrice("10.1");
+        stock.setUp("10%");
+        stock.setDate("2017-03-07");
+        stock.setTime("14:37:07");
+        stock.setBoughtPrice("9.0");
+        stock.setBoughtTime(new Date());
+        SwingValidateContext context = new SwingValidateContext(validateSwing, stock, null);
+        HttpMessage message = new HttpMessage();
+        message.setProtocol(HttpMessage.ProtocolEnum.HTTPS);
+        message.setUrl(new StringBuilder(config.getUrl()).append("?").append(config.getArg()).append("=").append(config.getToken()).toString());
+        message.setMessageTypeEnum(HttpMessage.MessageTypeEnum.markdown);
+        message.setContent(HttpMessage.MessageTypeEnum.markdown.createSaleContent(context));
+        boolean result = HttpUtil.sendMsg(message);
+        Assert.assertTrue(result);
     }
 }
