95179f5133ef7828a9316d414a817ff5ec731ff1
Justice-love <eddyxu1213@126.com>
2017-03-21 21:04:25 +0800

    增加aop登陆校验

--- web/src/main/java/org/eddy/aop/LoginCheckAspect.java
+++ web/src/main/java/org/eddy/aop/LoginCheckAspect.java
@@ -4,7 +4,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,11 +14,37 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoginCheckAspect {
 
+    public static final String FORBIDDEN = "forbidden";
+
     @Pointcut("execution(* org.eddy.web..*(..)) && (@annotation(org.eddy.annotation.LoginCheck))")
     public void loginCheckPointcut(){}
 
     @Around("loginCheckPointcut()")
     public Object check(ProceedingJoinPoint point) throws Throwable {
+        if (!loginCheck()) {
+            return checkFailAndReturn(point);
+        }
         return point.proceed();
+    }
+
+    private Object checkFailAndReturn(ProceedingJoinPoint point) throws Throwable {
+        if (! MethodSignature.class.isAssignableFrom(point.getSignature().getClass())) {
+            return point.proceed();
+        }
+        MethodSignature methodSignature = (MethodSignature) point.getSignature();
+        Class r = methodSignature.getReturnType();
+        if (r == String.class) {
+            return FORBIDDEN;
+        } else {
+            return point.proceed();
+        }
+    }
+
+    /**
+     *  登陆校验
+     * @return true:用户已登陆
+     */
+    private boolean loginCheck() {
+        return true;
     }
 }
