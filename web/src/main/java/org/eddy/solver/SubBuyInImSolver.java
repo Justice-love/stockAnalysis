6bcb2aaa4d26f18b848fadf15ff5c01e022971a5
Justice-love <eddyxu1213@126.com>
2017-03-29 16:27:14 +0800

    增加@Component注解

--- web/src/main/java/org/eddy/solver/SubBuyInImSolver.java
+++ web/src/main/java/org/eddy/solver/SubBuyInImSolver.java
@@ -2,10 +2,12 @@ package org.eddy.solver;
 
 import org.eddy.config.StockSolverConfig;
 import org.eddy.solver.imSolver.BuyInImSolver;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by eddy on 2017/3/29.
  */
+@Component("subBuyInImSolver")
 public class SubBuyInImSolver extends BuyInImSolver {
 
     @Override
