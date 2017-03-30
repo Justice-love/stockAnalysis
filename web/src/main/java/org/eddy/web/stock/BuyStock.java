74beb9bebe6a13bf700901c720561cab14427772
Justice-love <eddyxu1213@126.com>
2017-03-30 19:19:52 +0800

    增加详情页面

--- web/src/main/java/org/eddy/web/stock/BuyStock.java
+++ web/src/main/java/org/eddy/web/stock/BuyStock.java
@@ -5,6 +5,7 @@ import org.eddy.manager.StockBuyManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -37,5 +38,12 @@ public class BuyStock {
     public String show(Model model) {
         model.addAttribute("bought", stockBuyManager.showBought());
         return "stock/hadBought";
+    }
+
+    @RequestMapping("/showDetail/{code}")
+    @LoginCheck
+    public String showDetail(Model model, @PathVariable("code") String code) {
+        model.addAttribute("code", code);
+        return "stock/detail";
     }
 }
